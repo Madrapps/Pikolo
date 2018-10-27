a109aece13d3f06e3b56f9ecdc98fb518611bc29
Henry <th.saravana@gmail.com>
2018-10-27 23:52:51 +0530

    Fixing warnings

--- pikolo/src/main/java/com/madrapps/pikolo/Metrics.kt
+++ pikolo/src/main/java/com/madrapps/pikolo/Metrics.kt
@@ -2,8 +2,31 @@ package com.madrapps.pikolo
 
 import android.graphics.Paint
 import android.support.v4.graphics.ColorUtils
+import java.util.*
 
-data class Metrics(var centerX: Float = 0f, var centerY: Float = 0f, var hsl: FloatArray = floatArrayOf(0f, 1f, 0.5f), val density: Float)
+data class Metrics(var centerX: Float = 0f, var centerY: Float = 0f, var hsl: FloatArray = floatArrayOf(0f, 1f, 0.5f), val density: Float) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Metrics
+
+        if (centerX != other.centerX) return false
+        if (centerY != other.centerY) return false
+        if (!Arrays.equals(hsl, other.hsl)) return false
+        if (density != other.density) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = centerX.hashCode()
+        result = 31 * result + centerY.hashCode()
+        result = 31 * result + Arrays.hashCode(hsl)
+        result = 31 * result + density.hashCode()
+        return result
+    }
+}
 
 data class Paints(val shaderPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG), val indicatorPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG))
 
--- pikolo/src/main/java/com/madrapps/pikolo/components/HueComponent.kt
+++ pikolo/src/main/java/com/madrapps/pikolo/components/HueComponent.kt
@@ -1,8 +1,5 @@
 package com.madrapps.pikolo.components
 
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
 import android.support.v4.graphics.ColorUtils
 import com.madrapps.pikolo.Metrics
 import com.madrapps.pikolo.Paints
@@ -10,9 +7,9 @@ import com.madrapps.pikolo.Paints
 internal class HueComponent(metrics: Metrics, paints: Paints, override val arcLength: Float, override val arcStartAngle: Float) : ArcComponent(metrics, paints) {
     override val range: Float = 360f
     override val hslIndex: Int = 0
-    override val NO_OF_COLORS = 360
-    override val colors = IntArray(NO_OF_COLORS)
-    override val colorPosition = FloatArray(NO_OF_COLORS)
+    override val noOfColors = 360
+    override val colors = IntArray(noOfColors)
+    override val colorPosition = FloatArray(noOfColors)
 
     init {
         angle = (arcStartAngle + arcLength / 2f).toDouble()
@@ -21,7 +18,7 @@ internal class HueComponent(metrics: Metrics, paints: Paints, override val arcLe
     override fun getColorArray(hsl: FloatArray): IntArray {
         hsl[1] = 1f
         hsl[2] = 0.5f
-        for (i in 0..NO_OF_COLORS - 1) {
+        for (i in 0 until noOfColors) {
             hsl[hslIndex] = i.toFloat()
             colors[i] = ColorUtils.HSLToColor(hsl)
         }
