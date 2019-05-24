46fd3a9e471c243ae85e398cccae075b1c4bb03d
Henry <th.saravana@gmail.com>
2019-05-25 00:55:51 +0530

    Added Green and Blue components #25

--- pikolo/src/main/java/com/madrapps/pikolo/RGBColorPicker.kt
+++ pikolo/src/main/java/com/madrapps/pikolo/RGBColorPicker.kt
@@ -11,6 +11,8 @@ import android.view.MotionEvent
 import android.view.View
 import androidx.core.graphics.ColorUtils
 import com.madrapps.pikolo.components.ColorComponent
+import com.madrapps.pikolo.components.rgb.BlueComponent
+import com.madrapps.pikolo.components.rgb.GreenComponent
 import com.madrapps.pikolo.components.rgb.RedComponent
 import com.madrapps.pikolo.components.rgb.RgbMetrics
 import com.madrapps.pikolo.listeners.OnColorSelectionListener
@@ -22,8 +24,12 @@ open class RGBColorPicker @JvmOverloads constructor(context: Context, attrs: Att
     private val paints = Paints()
 
     private val redComponent: ColorComponent
+    private val greenComponent: ColorComponent
+    private val blueComponent: ColorComponent
 
     private val redRadiusOffset: Float
+    private val greenRadiusOffset: Float
+    private val blueRadiusOffset: Float
 
     init {
 
@@ -39,7 +45,15 @@ open class RGBColorPicker @JvmOverloads constructor(context: Context, attrs: Att
         val redArcLength = if (globalArcLength == 0f) 360f else globalArcLength
         val redStartAngle = 30f
 
+        val greenArcLength = if (globalArcLength == 0f) 360f else globalArcLength
+        val greenStartAngle = 150f
+
+        val blueArcLength = if (globalArcLength == 0f) 360f else globalArcLength
+        val blueStartAngle = 270f
+
         redComponent = RedComponent(metrics, paints, redArcLength, redStartAngle)
+        greenComponent = GreenComponent(metrics, paints, greenArcLength, greenStartAngle)
+        blueComponent = BlueComponent(metrics, paints, blueArcLength, blueStartAngle)
 
         redComponent.fillWidth = globalArcWidth
         redComponent.strokeWidth = globalStrokeWidth
@@ -50,10 +64,30 @@ open class RGBColorPicker @JvmOverloads constructor(context: Context, attrs: Att
 
         redRadiusOffset = if (globalRadiusOffset == 0f) dp(1f) else globalRadiusOffset
 
+        greenComponent.fillWidth = globalArcWidth
+        greenComponent.strokeWidth = globalStrokeWidth
+        greenComponent.indicatorStrokeWidth = globalIndicatorStrokeWidth
+        greenComponent.indicatorStrokeColor = globalIndicatorStrokeColor
+        greenComponent.strokeColor = globalStrokeColor
+        greenComponent.indicatorRadius = globalIndicatorRadius
+
+        greenRadiusOffset = if (globalRadiusOffset == 0f) dp(1f) else globalRadiusOffset
+
+        blueComponent.fillWidth = globalArcWidth
+        blueComponent.strokeWidth = globalStrokeWidth
+        blueComponent.indicatorStrokeWidth = globalIndicatorStrokeWidth
+        blueComponent.indicatorStrokeColor = globalIndicatorStrokeColor
+        blueComponent.strokeColor = globalStrokeColor
+        blueComponent.indicatorRadius = globalIndicatorRadius
+
+        blueRadiusOffset = if (globalRadiusOffset == 0f) dp(1f) else globalRadiusOffset
+
     }
 
     override fun onDraw(canvas: Canvas) {
         redComponent.drawComponent(canvas)
+        greenComponent.drawComponent(canvas)
+        blueComponent.drawComponent(canvas)
     }
 
     override fun onSizeChanged(width: Int, height: Int, oldW: Int, oldH: Int) {
@@ -62,27 +96,46 @@ open class RGBColorPicker @JvmOverloads constructor(context: Context, attrs: Att
         val outerRadius = minimumSize.toFloat() / 2f - padding
 
         redComponent.setRadius(outerRadius, redRadiusOffset)
+        greenComponent.setRadius(outerRadius, greenRadiusOffset)
+        blueComponent.setRadius(outerRadius, blueRadiusOffset)
 
         metrics.centerX = width / 2f
         metrics.centerY = height / 2f
 
         redComponent.updateComponent(redComponent.angle)
+        greenComponent.updateComponent(greenComponent.angle)
+        blueComponent.updateComponent(blueComponent.angle)
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
-        val isTouched = redComponent.onTouchEvent(event)
+        var isTouched = true
+        if (!redComponent.onTouchEvent(event)) {
+            if (!greenComponent.onTouchEvent(event)) {
+                isTouched = blueComponent.onTouchEvent(event)
+            }
+        }
         invalidate()
         return isTouched
     }
 
     fun setColorSelectionListener(listener: OnColorSelectionListener) {
         redComponent.setColorSelectionListener(listener)
+        greenComponent.setColorSelectionListener(listener)
+        blueComponent.setColorSelectionListener(listener)
     }
 
     open fun setColor(color: Int) {
         val red = Color.red(color).toFloat()
         metrics.color[0] = red
         redComponent.updateAngle(red)
+
+        val green = Color.green(color).toFloat()
+        metrics.color[1] = green
+        greenComponent.updateAngle(green)
+
+        val blue = Color.blue(color).toFloat()
+        metrics.color[2] = blue
+        blueComponent.updateAngle(blue)
         invalidate()
     }
 
